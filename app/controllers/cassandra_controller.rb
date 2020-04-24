68dc1f9bfa219d1024d1f5175fcb7ba634416c65
CSSE Department <csse@wootenjt.csse.rose-hulman.edu>
2020-04-23 23:31:22 -0500

    added cassandra connection info to /cassandra

--- app/controllers/cassandra_controller.rb
+++ app/controllers/cassandra_controller.rb
@@ -1,7 +1,10 @@
+require 'cassandra'
+
 class CassandraController < ApplicationController
     def index
         # add code for cassandra connection here
-
-        @data = "sample data to show"
+	@cluster = Cassandra.cluster(hosts: ['137.112.104.137', '137.112.104.136', '137.112.104.138'])
+	@name = @cluster.name
+	@hosts = @cluster.each_host
     end
 end
