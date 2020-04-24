8cd4418960ed966220be9ab3fe4fcc613045ab2e
CSSE Department <csse@wootenjt.csse.rose-hulman.edu>
2020-04-23 23:46:41 -0500

    added connection to neo4j

--- app/controllers/cassandra_controller.rb
+++ app/controllers/cassandra_controller.rb
@@ -1,4 +1,5 @@
 require 'cassandra'
+require 'neo4j/core/cypher_session/adaptors/http'
 
 class CassandraController < ApplicationController
     def index
@@ -6,5 +7,9 @@ class CassandraController < ApplicationController
 	@cluster = Cassandra.cluster(hosts: ['137.112.104.137', '137.112.104.136', '137.112.104.138'])
 	@name = @cluster.name
 	@hosts = @cluster.each_host
+
+	# code for neo4j connections
+	http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://neo4j:pass@localhost:7474')
+	neo4j_session = Neo4j::Core::CypherSession.new(http_adaptor)
     end
 end
