e2c49cf6edf847220060b5696a468def0260bb7b
Will Yelton <willhyelton@gmail.com>
2020-04-24 00:05:08 +0000

    added a basic outline for cassandra connection

---
+++ app/controllers/cassandra_controller.rb
@@ -0,0 +1,7 @@
+class CassandraController < ApplicationController
+    def index
+        # add code for cassandra connection here
+
+        @data = "sample data to show"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'sign_up/', to: 'sign_up#sign_up'
   get 'sign_up/total', to: 'sign_up#index'
 
+  get 'cassandra', to: 'cassandra#index'
+
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
