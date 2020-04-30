ae6d6cd901c5549d3e22a2f028b1b3c48865c7e0
burlea <burlea@rose-hulman.edu>
2020-04-30 00:39:48 -0400

    Added a method to get everything from schedule

--- app/services/schedule_services.rb
+++ app/services/schedule_services.rb
@@ -1,12 +1,15 @@
 require 'cassandra'
+
 class ScheduleService
+
+  @canOrderBy = %w[arrivingAt time train goingTo]
 
   def initialize()
     @cluster = Cassandra.cluster(hosts: ['137.112.104.137', '137.112.104.136', '137.112.104.138'])
     @stations = @cluster.connect("stations")
   end
 
-  def addToSchedule(arrivingAt,time,goingTo,train)
+  def addToSchedule(arrivingAt, time, goingTo, train)
     seeIfExists = @stations.prepare('SELECT * From arrivals Where arrivingAt = ? and time = ? and train = ? ALLOW FILTERING;')
     addTrainTime = @stations.prepare('INSERT INTO arrivals (arrivingAt, time, goingTo, train) VALUES (?,?,?,?)')
 
@@ -15,22 +18,29 @@ class ScheduleService
     if not duplicates.empty?
       return "This station, time, and train combination already exists";
     else
-      @stations.execute(addTrainTime, arguments: [arrivingAt,time,goingTo,train])
+      @stations.execute(addTrainTime, arguments: [arrivingAt, time, goingTo, train])
       return ""
     end
   end
 
-  def removeFromSchedule(arrivingAt,time,train)
+  def removeFromSchedule(arrivingAt, time, train)
     seeIfExists = @stations.prepare('SELECT * From arrivals Where arrivingAt = ? and time = ? and train = ? LIMIT 1 ALLOW FILTERING;')
     removeTrainTime = @stations.prepare('Delete From arrivals Where arrivingAt = ? and time = ? and train = ?')
 
     exists = @stations.execute(seeIfExists, arguments: [arrivingAt, time, train])
     if not exists.empty?
-      @stations.execute(removeTrainTime, arguments: [arrivingAt,time,train])
+      @stations.execute(removeTrainTime, arguments: [arrivingAt, time, train])
       return ""
     else
       return "This station, time, and train combination does not exist";
     end
   end
+
+  def getScheduale ()
+    search = @stations.prepare("Select * From arrivals")
+    results = @stations.execute(search)
+    return results
+  end
+
 
 end
