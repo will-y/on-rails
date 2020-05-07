b39a9d6a38ed5372699a6747ddbf7f4da61dcbe4
burlea <burlea@rose-hulman.edu>
2020-05-07 18:26:23 -0400

    Finished backend methods for editing a schedule, filtering a schedule, and getting a speciic row

--- app/services/schedule_service.rb
+++ app/services/schedule_service.rb
@@ -5,21 +5,33 @@ class ScheduleService
   @canOrderBy = %w[arrivingAt time train goingTo]
 
   def initialize
-    @cluster = Cassandra.cluster(hosts: ['137.112.104.137', '137.112.104.136', '137.112.104.138'])
+    @cluster = Cassandra.cluster(hosts: %w[137.112.104.137 137.112.104.136 137.112.104.138])
     @stations = @cluster.connect("stations")
   end
 
-  def addToSchedule(arrivingAt, time, goingTo, train)
+  def addToSchedule(arrivingAt, time, goingTo, train, price)
     seeIfExists = @stations.prepare('SELECT * From arrivals Where arrivingAt = ? and time = ? and train = ? ALLOW FILTERING;')
-    addTrainTime = @stations.prepare('INSERT INTO arrivals (arrivingAt, time, goingTo, train) VALUES (?,?,?,?)')
+    addTrainTime = @stations.prepare('INSERT INTO arrivals (arrivingAt, time, goingTo, train, price) VALUES (?,?,?,?,?)')
 
     duplicates = @stations.execute(seeIfExists, arguments: [arrivingAt, time, train])
-    puts(duplicates.empty?)
     if not duplicates.empty?
       return "This station, time, and train combination already exists";
     else
-      @stations.execute(addTrainTime, arguments: [arrivingAt, time, goingTo, train])
+      @stations.execute(addTrainTime, arguments: [arrivingAt, time, goingTo, train, price])
       return ""
+    end
+  end
+
+  def editSchedule (oldArrivingAt, oldTime, oldTrain, newArrivingAt, newTime, newTrain, newGoingTo, newPrice)
+    seeIfExists = @stations.prepare('SELECT * From arrivals Where arrivingAt = ? and time = ? and train = ? LIMIT 1 ALLOW FILTERING;')
+
+    exists = @stations.execute(seeIfExists, arguments: [oldArrivingAt, oldTime, oldTrain])
+    if not exists.empty?
+      removeFromSchedule(oldArrivingAt,oldTime,oldTrain)
+      addToSchedule(newArrivingAt,newTime,newGoingTo,newTrain,newPrice)
+      return ""
+    else
+      return "This station, time, and train combination does not exist";
     end
   end
 
@@ -43,9 +55,57 @@ class ScheduleService
   end
 
   def get_row(arrivingAt, time, train)
-    #TODO: Give the single record that matches this key
-    {arrivingat: "ex", "time": "ex", train: "train", goingto: "ex", price: 12, price_1st:20}.with_indifferent_access
+    search = @stations.prepare("Select * From arrivals Where arrivingAt = ? and time = ? and train = ? ALLOW FILTERING;")
+    results = @stations.execute(search, arguments: [arrivingAt, time, train])
+    return results;
   end
 
+
+  def filter (arrivingAt, time, train, goingTo)
+    query = "Select * From arrivals"
+    isFirstElement = true;
+
+    if arrivingAt != ''
+      if isFirstElement
+        query = query + " Where arrivingAt = '" + arrivingAt + "'"
+        isFirstElement = false;
+      else
+        query = query + " and arrivingAt = '" + arrivingAt + "'"
+      end
+    end
+
+    if time != ''
+      if isFirstElement
+        query = query + " Where time = '" + time + "'"
+        isFirstElement = false;
+      else
+        query = query + " and time = '" + time + "'"
+      end
+    end
+
+    if train != ''
+      if isFirstElement
+        query = query + " Where train = '" + train + "'"
+        isFirstElement = false;
+      else
+        query = query + " and train = '" + train + "'"
+      end
+    end
+
+    if goingTo != ''
+      if isFirstElement
+        query = query + " Where goingTo = '" + goingTo + "'"
+        isFirstElement = false;
+      else
+        query = query + " and goingTo = '" + goingTo + "'"
+      end
+    end
+
+    search = @stations.prepare(query + " allow filtering;")
+    results = @stations.execute(search)
+
+    return results
+
+  end
 
 end
