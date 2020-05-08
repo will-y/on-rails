63393f516dcb2049464f614bc3ac700acb47f84a
Will Yelton <willhyelton@gmail.com>
2020-05-08 01:35:57 -0400

    buying tickets now works

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,10 +4,27 @@ class TicketsController < ApplicationController
     service = ScheduleService.new
     @ticket = Ticket.new
     @schedule = service.get_row(params["arrivingat"], params["time"], params["train"])
-    @price = @schedule[:price]
-    @price_1st = @schedule[:price_1st]
-    @going_to = @schedule[:goingto]
+    @schedule.each do |row|
+        @price = row["price"]
+        @going_to = row["goingto"]
+    end
+
     @states = %w(Alaska Alabama Arkansas American\ Samoa Arizona California Colorado Connecticut District\ of\ Columbia Delaware Florida Georgia Guam Hawaii Iowa Idaho Illinois Indiana Kansas Kentucky Louisiana Massachusetts Maryland Maine Michigan Minnesota Missouri Mississippi Montana North\ Carolina North\ Dakota Nebraska New\ Hampshire New\ Jersey New\ Mexico Nevada New\ York Ohio Oklahoma Oregon Pennsylvania Puerto\ Rico Rhode\ Island South\ Carolina South\ Dakota Tennessee Texas Utah Virginia Virgin\ Islands Vermont Washington Wisconsin West\ Virginia Wyoming)
     @user = @current_user
   end
+
+  def create
+    #@ticket = Ticket.new(ticket_params)
+    #@current_user.tickets.push(@ticket)
+    #@ticket.user = @user
+    #@ticket.save
+    #@current_user.save
+    @current_user.tickets.create(ticket_params)
+    redirect_to schedules_path
+  end
+
+  private
+    def ticket_params
+      params.require(:ticket).permit(:origin, :destination, :train, :price, :first_class, :time, :user)
+    end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,11 +1,11 @@
 class Ticket
   include Mongoid::Document
 
-  field :origin
-  field :destination
-  field :train
-  field :price
-  field :first_class
-  field :time
+  field :origin, type: String
+  field :destination, type: String
+  field :train, type: String
+  field :price, type: String
+  field :first_class, type: Boolean
+  field :time, type: String
   belongs_to :user
 end
