210a4109a3de0de962588cc634504022503af34e
Will Yelton <willhyelton@gmail.com>
2020-05-08 02:13:11 -0400

    added filtering

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,7 +2,8 @@ class SchedulesController < ApplicationController
 
   def index
     service = ScheduleService.new
-    @schedule = service.getSchedule
+    @schedule = service.filter(params[:origin], params[:time], params[:train], params[:destination])
   end
+
 end
 
